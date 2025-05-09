0303dbf80a3d9c72f3c4984df9755c454ffd9a75
Abdullah <36683801+Abdullah221@users.noreply.github.com>
2025-05-09 17:01:49 +0500

    Todo-App-Version-1

--- js/app.js
+++ js/app.js
@@ -1,24 +1,50 @@
-const currentUser = JSON.parse(localStorage.getItem("currentUser"));
-if(!currentUser){
-    window.location.href="../index.html";
-};
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+    if(!currentUser){
+        window.location.href="../index.html";
+    };
 
-const userName = document.getElementById("username");
-userName.textContent = currentUser.username;
+    const userName = document.getElementById("username");
+    userName.textContent = currentUser.username;
+        
+
+    const todoForm = document.getElementById("todo-form");
+    const ul = document.getElementById("todo-list");
+
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    function render(){
+    const usertask = tasks.filter(user => user.email === currentUser.email);
     
-
-const todoForm = document.getElementById("todo-form");
-const ul = document.getElementById("todo-list");
-todoForm.addEventListener("submit", (e)=> {
-    e.preventDefault();
-
-    const task = e.target.task.value;
-    
-
-
-    e.target.reset();
-});
-function logout(){
-    localStorage.removeItem("currentUser");
-    window.location.href = "../index.html"
+    usertask.forEach(taskObj => {
+        const li = document.createElement("li");
+        li.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center");
+        li.innerHTML = taskObj.task;
+        ul.appendChild(li);
+    });
 }
+render();
+    todoForm.addEventListener("submit", (e)=> {
+        e.preventDefault();
+
+        const task = e.target.task.value;
+
+        const taskObj = {
+            task: task,
+            email: currentUser.email,
+        }
+        
+        
+        tasks.push(taskObj);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        const li = document.createElement("li");
+        li.setAttribute("class", "list-group-item d-flex justify-content-between align-items-center");
+        li.innerHTML = taskObj.task;
+        ul.appendChild(li);
+        
+        e.target.reset();
+    });
+    function logout(){
+        localStorage.removeItem("currentUser");
+        window.location.href = "../index.html"
+    }
